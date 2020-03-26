2b6f4a0cd4f52639aca3d9331c078960dbe0c294
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-03-26 07:09:54 -0300

    Transferred widgets to useful_widgets
    The follow widgets will be transferred to useful_widgets package:
    `LoginWidget`, `FacebookLoginButton`, `GitHubLoginButton`, `GoogleLoginButton`, `MicrosoftLoginButton`, `TwitterLoginButton`, `DefaultSocialButtonLogin`

--- lib/user_structure.dart
+++ lib/user_structure.dart
@@ -3,12 +3,6 @@ library user_structure;
 export 'localizations/localizations_enums.dart';
 export 'localizations/user_structure_localization.dart';
 
-export 'buttons/facebook_login_button.dart';
-export 'buttons/github_login_button.dart';
-export 'buttons/google_login_button.dart';
-export 'buttons/microsoft_login_button.dart';
-export 'buttons/twitter_login_button.dart';
-
 export 'classes/storage_uploader_manager.dart';
 
 export 'models/pattern_model.dart';
@@ -18,7 +12,6 @@ export 'models/user_auth_model_pattern.dart';
 export 'models/user_model_pattern.dart';
 
 export 'widgets/avatar/avatar_widget.dart';
-export 'widgets/login/login_widget.dart';
 export 'widgets/sign_up/sign_up_step_widget.dart';
 export 'widgets/sign_up/sign_up_widget.dart';
 export 'widgets/user_profile/user_profile_widget.dart';
