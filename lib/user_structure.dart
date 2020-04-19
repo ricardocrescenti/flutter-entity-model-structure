f25f6c35bd66b21719ed738149daae27d92432e8
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-04-19 09:28:50 -0300

    added user tile widget

--- lib/user_structure.dart
+++ lib/user_structure.dart
@@ -15,3 +15,4 @@ export 'widgets/avatar/avatar_widget.dart';
 export 'widgets/sign_up/sign_up_step_widget.dart';
 export 'widgets/sign_up/sign_up_widget.dart';
 export 'widgets/user_profile/user_profile_widget.dart';
+export 'widgets/user_tile/user_tile_widget.dart';
