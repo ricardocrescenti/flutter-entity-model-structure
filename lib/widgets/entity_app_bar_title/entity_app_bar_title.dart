ef045404cbe7f10f207fa29552e3ee5c6d6f482e
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-08-07 15:03:09 -0300

    Changed input parameters

--- lib/widgets/entity_app_bar_title/entity_app_bar_title.dart
+++ lib/widgets/entity_app_bar_title/entity_app_bar_title.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:user_structure/user_structure.dart';
 
 class EntityAppBarTitle extends StatelessWidget {
+  final Widget title;
   final EntityModelPattern entity;
 
-  EntityAppBarTitle(this.entity);
+  EntityAppBarTitle({
+    this.title,
+    @required this.entity,
+  }) {
+    assert(entity != null);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        (entity == null ? Container() : Padding(
+        Padding(
           padding: EdgeInsets.only(right: 10),
-          child: AvatarWidget(avatar: entity.photo?.getPublicUrl(size: 75), size: 20,))
+          child: AvatarWidget(avatar: entity.photo?.getPublicUrl(size: 75), size: 20)
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Shop'),
-            (entity == null ? Container() : Text(entity.name, style: Theme.of(context).textTheme.subtitle))
+            title,
+            (entity == null ? Container() : Text(entity.name, style: Theme.of(context).textTheme.subtitle2))
           ],
         ),
       ]
